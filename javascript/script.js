391ee05b820b4b584b0380f336a05fe6589a7998
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-01-16 18:45:27 -0500

    playing with boxes

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,12 @@
 
-
+//attempting to use NPM module to use moment.js - ended up using script source from browser
 // const fs = require("fs");
 // const moment = require("../../node_modules/moment/moment");
 // var fs = require('fs');
 // var moment = require('moment'); 
 // moment().format(); 
 
+//A function to show today's date.
 const todayDay = () => {
 const today = document.getElementById("today");
 console.log(today);
@@ -13,7 +14,7 @@ var dayToday = `Today's Date is ${moment().format("dddd, MMMM Do YYYY")}`;
 today.append(dayToday);
 }
 todayDay();
-
+//and a function to add the number of days since I started learning to code.
 const since = () =>{
   let beginCoding = moment("2021-12-06", "YYYY-MM-DD");
   let currentDay = moment().startOf('day');
@@ -23,16 +24,5 @@ today.append(`; it has been ${daysSince} days since I started learning to code.`
 console.log(daysSince);
 }
 since();
-
-
-
-// const dayToday = moment().format("dddd, MMMM Do YYYY");
-// today.text(dayToday);
-
-
-// // const todayDate = moment().format("[Today is] dddd");
-// // today.text(today);
-
-// // console.log(todayDate);
 
 
